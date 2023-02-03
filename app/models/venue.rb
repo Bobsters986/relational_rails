e977e73c49a533e98388b7963dab8fb09bfe59e7
Bobby Luly <bobbyluly@gmail.com>
2023-02-02 21:41:59 -0700

    Feat: create #artist_count action and use it in parent show page

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,6 +2,10 @@ class Venue < ApplicationRecord
   has_many :artists, dependent: :destroy
 
   def self.sort
-    order(:created_at)
+    order(created_at: :desc)
+  end
+
+  def artist_count
+    self.artists.count
   end
 end
