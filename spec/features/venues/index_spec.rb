5f42b24b4d7e6711706c3d28a9db14d639e00275
Bobby Luly <bobbyluly@gmail.com>
2023-02-02 17:08:02 -0700

    Test: create test for user story 6

--- spec/features/venues/index_spec.rb
+++ spec/features/venues/index_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe "Venues Index Page", type: :feature do
   # As a visitor
   # When I visit '/parents'
   # Then I see the name of each parent record in the system
-  let!(:venue_1) { Venue.create!(decible_limit: true, rank:1, name: "Red Rocks") }
-  let!(:venue_2) { Venue.create!(decible_limit: false, rank:2, name: "Mission Ballroom") }
+  let!(:venue_1) { Venue.create!(decible_limit: true, rank:1, name: "Red Rocks", created_at: Time.now - 1.hour) }
+  let!(:venue_2) { Venue.create!(decible_limit: false, rank:2, name: "Mission Ballroom", created_at: Time.now - 2.hour) }
   let!(:venue_3) { Venue.create!(decible_limit: false, rank:5, name: "Ball Arena") }
 
   describe "As a visitor" do
@@ -19,6 +19,18 @@ RSpec.describe "Venues Index Page", type: :feature do
         expect(page).to have_content(venue_1.name)
         expect(page).to have_content(venue_2.name)
         expect(page).to have_content(venue_3.name)
+      end
+
+      it "I see that records are ordered by most recently created first" do
+
+        visit '/venues'
+        save_and_open_page
+        
+        expect(page).to have_content(venue_1.created_at)
+        expect(page).to have_content(venue_2.created_at)
+        expect(page).to have_content(venue_3.created_at)
+        expect(venue_2.name).to appear_before(venue_1.name)
+        expect(venue_1.name).to appear_before(venue_3.name)
       end
     end
   end
