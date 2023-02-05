5a28f042465d578c6d7b4ec0a63287ca1cf9e5a5
Bobby Luly <bobbyluly@gmail.com>
2023-02-04 22:38:50 -0700

    Test: add tests for user story 11

--- spec/features/venues/new_spec.rb
+++ spec/features/venues/new_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a new venue' do
-  # Then I see a link to create a new Parent record, "New Parent"
-  # When I click this link
-  # Then I am taken to '/parents/new' where I  see a form for a new parent record
-  # When I fill out the form with a new parent's attributes:
-  # And I click the button "Create Parent" to submit the form
-  # Then a `POST` request is sent to the '/parents' route,
-  # a new parent record is created,
-  # and I am redirected to the Parent Index page where I see the new Parent displayed.
-
   describe "As a visitor" do
     describe "when I visit '/venues' " do
+      # Then I see a link to create a new Parent record, "New Parent"
+      # When I click this link
+      # Then I am taken to '/parents/new' where I  see a form for a new parent record
+      # When I fill out the form with a new parent's attributes:
+      # And I click the button "Create Parent" to submit the form
+      # Then a `POST` request is sent to the '/parents' route,
+      # a new parent record is created,
+      # and I am redirected to the Parent Index page where I see the new Parent displayed.
+      
       it '11. I can click a link to create a new parent, and I am taken to a page with a form to create that parent' do
         visit '/venues'
 
