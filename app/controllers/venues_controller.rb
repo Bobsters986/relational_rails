490af2d33bb4b9b4ab066d34f59d29230f64d93a
Bobby Luly <bobbyluly@gmail.com>
2023-02-04 21:14:24 -0700

    Add all functionality to add a new venue

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -3,7 +3,20 @@ class VenuesController < ApplicationController
     @venues = Venue.sort
   end
 
+  def new
+  end
+
+  def create
+    venue = Venue.create(venue_params)
+    redirect_to '/venues'
+  end
+
   def show
     @venue = Venue.find(params[:id])
+  end
+
+  private
+  def venue_params
+    params.permit(:name, :decible_limit, :rank)
   end
 end
